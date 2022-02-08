Added integrated testing, fixed Jacobian approx with finite

UQtoolbox_examples.py
@@ -13,136 +13,137 @@ def GetExample(example, **kwargs):
     # Outputs: model and options objects corresponding to the desired model
 
     # Initialize options object
-    options = uq.uqOptions()
+    options = uq.Options()
     # Select Example model
     if example.lower() == 'linear':
         #baseEvalPoints = np.array([0, .5, 1, 2])  # Requires 1xnQOIs indexing
-        baseEvalPoints= np.array([1])
-        model = uq.model(evalFcn=lambda params: linear_function(baseEvalPoints, params),
-                         basePOIs=np.array([1, 1]),
+        baseEvalPoints= np.array([2])
+        model = uq.Model(eval_fcn=lambda params: linear_function(baseEvalPoints, params),
+                         base_poi=np.array([1, 1]),
                          cov=np.array([[1, 0], [0, 1]]),
                          dist='uniform',
-                         distParms=np.array([[0], [1]])*np.array([1, 1])
-                         #distParms=np.array([[.9999999999], [1.0000000001]])*np.array([1, 1])
+                         dist_param=np.array([[0], [1]])*np.array([1, 1])
+                         #dist_param=np.array([[.9999999999], [1.0000000001]])*np.array([1, 1])
                          )
     elif example.lower() == 'quadratic':
         baseEvalPoints = np.array([0, .5, 1, 2])  # Currently requires 1xn or nx1 ordering
-        model = uq.model(evalFcn=lambda params: quadratic_function(baseEvalPoints, params),
-                         basePOIs=np.array([1, 1, 1]),
+        model = uq.Model(eval_fcn=lambda params: quadratic_function(baseEvalPoints, params),
+                         base_poi=np.array([1, 1, 1]),
                          cov=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
         options.gsa.nSampSobol=100               # Keep normal sampling but reduce sample size to 100
     elif example.lower() == 'helmholtz':
         baseEvalPoints = np.arange(0, .801, .05)
-        model = uq.model(evalFcn=lambda params: HelmholtzEnergy(baseEvalPoints, params),
-                         basePOIs=np.array([-392.66, 770.1, 57.61]),
+        model = uq.Model(eval_fcn=lambda params: HelmholtzEnergy(baseEvalPoints, params),
+                         base_poi=np.array([-392.66, 770.1, 57.61]),
                          cov=np.array([[0.0990, - 0.4078, 0.4021],  # Covaraince matrix calculated by DRAMs
                                        [-0.4078, 2.0952, -2.4078],  # at baseParams and basEvalPoints
                                        [0.4021, -2.4078, 3.0493]]) * (10 ** 3),
+                         poi_name = np.array(["alpha1", "alpha11", "alpha111"]),
                          dist='uniform')  # Use uniform sampling of +-20% nominal value
-        model.distParms = np.array([[.8, .8, .8], [1.2, 1.2, 1.2]]) * model.basePOIs
+        model.dist_param = np.array([[.8, .8, .8], [1.2, 1.2, 1.2]]) * model.base_poi
         options.gsa.nSampSobol=10000              # Keep normal sampling but reduce sample size to 100
     elif example.lower() == 'integrated helmholtz':
-        model = uq.model(evalFcn=lambda params: IntegratedHelmholtzEnergy(np.arange(0,.8,.06), params),
-                         basePOIs=np.array([-389.4, 761.3, 61.5]),
+        model = uq.Model(eval_fcn=lambda params: IntegratedHelmholtzEnergy(np.arange(0,.8,.06), params),
+                         base_poi=np.array([-389.4, 761.3, 61.5]),
                          cov=np.array([[0.0990, - 0.4078, 0.4021],  # Covaraince matrix calculated by DRAMs
                                        [-0.4078, 2.0952, -2.4078],  # at baseParams and basEvalPoints
                                        [0.4021, -2.4078, 3.0493]]) * (10 ** 3),
-                         POInames=np.array(["alpha_1", "alpha11", "alpha111"]),
-                         QOInames=np.array(["x=.8", "x=.80001"]),
+                         name_poi=np.array(["alpha_1", "alpha11", "alpha111"]),
+                         name_qoi=np.array(["x=.8", "x=.80001"]),
                          dist="uniform")  # Use uniform sampling of +-20% nominal value
-        model.distParms = np.array([[.8, .8, .8], [1.2, 1.2, 1.2]]) * model.basePOIs
-        #model.distParms = np.array([[.999999, .999999, .999999], [1.000001, 1.000001, 1.000001]]) * model.basePOIs
+        model.dist_param = np.array([[.8, .8, .8], [1.2, 1.2, 1.2]]) * model.base_poi
+        #model.dist_param = np.array([[.999999, .999999, .999999], [1.000001, 1.000001, 1.000001]]) * model.base_poi
     elif example.lower() == 'linear product':  # Linear product example taken from Homma1996
-        model = uq.model(evalFcn=LinearProd,
-                         basePOIs=np.array([.5, .5, .5, .5, .5]),
+        model = uq.Model(eval_fcn=LinearProd,
+                         base_poi=np.array([.5, .5, .5, .5, .5]),
                          dist="uniform",
-                         distParms=np.array([[0, 0, 0, 0, 0], [1, 1, 1, 1, 1]]))
+                         dist_param=np.array([[0, 0, 0, 0, 0], [1, 1, 1, 1, 1]]))
     elif example.lower() == 'ishigami (uniform)':
-        model = uq.model(evalFcn=Ishigami,
-                         basePOIs=np.array([0, 0, 0]),
+        model = uq.Model(eval_fcn=Ishigami,
+                         base_poi=np.array([0, 0, 0]),
                          dist="uniform",
-                         distParms=np.array([[-math.pi, -math.pi, -math.pi], [math.pi, math.pi, math.pi]]))
+                         dist_param=np.array([[-math.pi, -math.pi, -math.pi], [math.pi, math.pi, math.pi]]))
         options.lsa.method = 'finite' 
         options.lsa.xDelta = 10**(-6)
         options.gsa.nSampSobol = 4000000          # Use default number of samples
         options.path='..\\Figures\\Ishigami(uniform)'
     elif example.lower() == 'ishigami (normal)':
-        model = uq.model(evalFcn=Ishigami,
-                         basePOIs=np.array([0, 0, 0]),
+        model = uq.Model(eval_fcn=Ishigami,
+                         base_poi=np.array([0, 0, 0]),
                          dist="SaltelliNormal",
-                         distParms=np.array([[0, 0, 0], [(2*math.pi)**2/12, (2*math.pi)**2/12, (2*math.pi)**2/12]]))
+                         dist_param=np.array([[0, 0, 0], [(2*math.pi)**2/12, (2*math.pi)**2/12, (2*math.pi)**2/12]]))
         options.lsa.method = 'finite' 
         options.lsa.xDelta = 10**(-6)
         options.gsa.nSampSobol= 4000000          # Use default number of samples
         options.path='..\\Figures\\Ishigami(normal)'
     elif example.lower() == 'trial function':
-        model = uq.model(evalFcn=TrialFunction,
-                         basePOIs=np.array([1, 1, 1]),
+        model = uq.Model(eval_fcn=TrialFunction,
+                         base_poi=np.array([1, 1, 1]),
                          dist="uniform",
-                         distParms=np.array([[1, 1, 1], [1000, 100, 10]])
+                         dist_param=np.array([[1, 1, 1], [1000, 100, 10]])
                          )
     elif example.lower() == 'portfolio (normal)':
-        model=uq.model(evalFcn=lambda params: Portfolio(params, np.array([2, 1])),
-                       basePOIs=np.array([0, 0]),
+        model=uq.Model(eval_fcn=lambda params: Portfolio(params, np.array([2, 1])),
+                       base_poi=np.array([0, 0]),
                        dist="normal",
-                       distParms=np.array([[0, 0], [1, 9]]))
+                       dist_param=np.array([[0, 0], [1, 9]]))
         options.gsa.nSampSobol = 100000
     elif example.lower() == 'portfolio (uniform)':
-        model=uq.model(evalFcn=lambda params: Portfolio(params, np.array([2, 1])),
-                       basePOIs=np.array([0, 0]),
+        model=uq.Model(eval_fcn=lambda params: Portfolio(params, np.array([2, 1])),
+                       base_poi=np.array([0, 0]),
                        dist="uniform",
-                       distParms=np.array([[-np.sqrt(12)/2, -3*np.sqrt(3)], [np.sqrt(12)/2, 3*np.sqrt(3)]]))
+                       dist_param=np.array([[-np.sqrt(12)/2, -3*np.sqrt(3)], [np.sqrt(12)/2, 3*np.sqrt(3)]]))
         options.path = '..\\Figures\\Portfolio(Uniform)'
         options.gsa.nSampSobol = 2**12
     elif example.lower() == 'aluminum rod (uniform)':
-        model = uq.model(evalFcn=lambda params: HeatRod(params, np.array([55])),
-                         basePOIs=np.array([-18.4, .00191]),
+        model = uq.Model(eval_fcn=lambda params: HeatRod(params, np.array([55])),
+                         base_poi=np.array([-18.4, .00191]),
                          dist="uniform",
-                         distParms=np.array([[-18.4-(.1450*np.sqrt(3)), .00191-(1.4482*(10**(-5))*np.sqrt(3))],\
+                         dist_param=np.array([[-18.4-(.1450*np.sqrt(3)), .00191-(1.4482*(10**(-5))*np.sqrt(3))],\
                                              [-18.4+(.1450*np.sqrt(3)), .00191+(1.4482*(10**(-5))*np.sqrt(3))]]),
-                         POInames=np.array(['Phi', 'h']),
-                         QOInames=np.array(['T(x=55)']))
+                         name_poi=np.array(['Phi', 'h']),
+                         name_qoi=np.array(['T(x=55)']))
         options.path = '..\\Figures\\AluminumRod(Uniform, x=55)'
         options.gsa.nSampSobol = 500000
     elif example.lower() == 'aluminum rod (normal)':
-        model = uq.model(evalFcn=lambda params: HeatRod(params, np.array([15, 25, 35, 45, 55])),
-                         basePOIs=np.array([-18.4, .00191]),
+        model = uq.Model(eval_fcn=lambda params: HeatRod(params, np.array([15, 25, 35, 45, 55])),
+                         base_poi=np.array([-18.4, .00191]),
                          dist='normal',
-                         distParms=np.array([[-18.4, .00191], [.1450**2, (1.4482*10**(-5))**2]]),
-                         POInames=np.array(['Phi', 'h']),
-                         #QOInames=np.array(['T(x=15)','T(x=55)'])
+                         dist_param=np.array([[-18.4, .00191], [.1450**2, (1.4482*10**(-5))**2]]),
+                         name_poi=np.array(['Phi', 'h']),
+                         #name_qoi=np.array(['T(x=15)','T(x=55)'])
                          )
         options.path = '..\\Figures\\AluminumRod(Normal)'
         options.gsa.nSampSobol = 100000000
     elif example.lower() == 'aluminum rod (saltelli normal)':
-        model = uq.model(evalFcn=lambda params: HeatRod(params, np.array([55])),
-                         basePOIs=np.array([-18.4, .00191]),
+        model = uq.Model(eval_fcn=lambda params: HeatRod(params, np.array([55])),
+                         base_poi=np.array([-18.4, .00191]),
                          dist="SaltelliNormal",
-                         distParms=np.array([[-18.4, .00191], [.1450**2, (1.4482*10**(-5))**2]]),
-                         POInames=np.array(['Phi', 'h']),
-                         QOInames=np.array(['T(x=55)']))
+                         dist_param=np.array([[-18.4, .00191], [.1450**2, (1.4482*10**(-5))**2]]),
+                         name_poi=np.array(['Phi', 'h']),
+                         name_qoi=np.array(['T(x=55)']))
         options.path = '..\\Figures\\AluminumRod(SaltelliNormal, x=55)'
         options.gsa.nSampSobol = 200000
     elif example.lower() == 'sir infected':
-        model = uq.model(evalFcn = lambda params: SolveSIRinfected(params, np.array([960, 40, 0]), np.array([0, 1, 3, 5, 6])),
-                         basePOIs=np.array([8, 1.5]),
-                         POInames=np.array(['beta', 'gamma']),
+        model = uq.Model(eval_fcn = lambda params: SolveSIRinfected(params, np.array([960, 40, 0]), np.array([0, 1, 3, 5, 6])),
+                         base_poi=np.array([8, 1.5]),
+                         name_poi=np.array(['beta', 'gamma']),
                          dist='uniform',
-                         distParms=np.array([[0, 0], [1, 1]])
+                         dist_param=np.array([[0, 0], [1, 1]])
                          )
         options.lsa.method='finite'
         options.lsa.xDelta=.00001
     elif example.lower() == 'sobol test function':
-        model = uq.model(evalFcn= lambda params: SobolTestFunction(params,np.array([78, 12, .5, 2, 97, 33])),
-                         basePOIs= np.array([.5, .5, .5, .5, .5, .5]),
+        model = uq.Model(eval_fcn= lambda params: SobolTestFunction(params,np.array([78, 12, .5, 2, 97, 33])),
+                         base_poi= np.array([.5, .5, .5, .5, .5, .5]),
                          dist='uniform',
-                         distParms=np.array([[0,0,0,0,0,0], [1,1,1,1,1,1]]))
+                         dist_param=np.array([[0,0,0,0,0,0], [1,1,1,1,1,1]]))
     else:
         raise Exception("Unrecognized Example Type")
 
     # Apply optional inputs
     if 'basePOI' in kwargs:  # Change base parameter values to input
-        model.basePOIs = kwargs['basePOI']
+        model.base_poi = kwargs['basePOI']
     if 'evalPoints' in kwargs:  # Determine eval points
         model.evalPoints = kwargs['evalPoints']
 

lsa.py
@@ -74,9 +74,11 @@ def run_lsa(model, lsa_options):
     # Outputs: Object of class lsa with Jacobian, RSI, and Fisher information matrix
 
     # Calculate Jacobian
-    jac_raw=get_jacobian(model.eval_fcn, model.base_poi, lsa_options.x_delta, lsa_options.method, scale=False, y_base=model.base_qoi)
+    jac_raw=get_jacobian(model.eval_fcn, model.base_poi, lsa_options.x_delta,\
+                         lsa_options.method, scale=False, y_base=model.base_qoi)
     # Calculate relative sensitivity index (RSI)
-    jac_rsi=get_jacobian(model.eval_fcn, model.base_poi, lsa_options, scale=True, y_base=model.base_qoi)
+    jac_rsi=get_jacobian(model.eval_fcn, model.base_poi, lsa_options.x_delta,\
+                         lsa_options.method, scale=True, y_base=model.base_qoi)
     # Calculate Fisher Information Matrix from jacobian
     fisher_mat=np.dot(np.transpose(jac_raw), jac_raw)
 
@@ -127,7 +129,8 @@ def get_jacobian(eval_fcn, x_base, x_delta, method, **kwargs):
     else:
         scale = False                                                       # Function defaults to no scaling
     if 'y_base' in kwargs:
-        y_base = kwargs["y_base"]
+        y_base = eval_fcn(x_base)
+        #y_base = kwargs["y_base"]
         # Make sure x_base is int/ float and convert to numpy array
         if type(y_base)==int or type(y_base)==float:
             y_base = np.array([y_base])
@@ -172,13 +175,20 @@ def get_jacobian(eval_fcn, x_base, x_delta, method, **kwargs):
             xPert = x_base + np.zeros(shape=x_base.shape)*1j                  # Initialize Complex Perturbed input value
             xPert[i_poi] += x_delta * 1j                                      # Add complex Step in input
         elif method.lower() == 'finite':
-            xPert=x_base*(1+x_delta)
+            xPert = x_base.copy()
+            xPert[i_poi] += x_delta
         yPert = eval_fcn(xPert)                                        # Calculate perturbed output
         for i_qoi in range(0, n_qoi):                                        # Loop through QOIs
             if method.lower()== 'complex':
                 jac[i_qoi, i_poi] = np.imag(yPert[i_qoi] / x_delta)                 # Estimate Derivative w/ 2nd order complex
             elif method.lower() == 'finite':
+                print("xPert" + str(xPert))
+                print("x_base" + str(x_base))
+                print("yPert" + str(yPert))
+                print("y_base" + str(y_base))
+                print("x_delta" + str(x_delta))
                 jac[i_qoi, i_poi] = (yPert[i_qoi]-y_base[i_qoi]) / x_delta
+                print("Deriv: " + str(jac[i_qoi, i_poi]))
             #Only Scale Jacobian if 'scale' value is passed True in function call
             if scale:
                 jac[i_qoi, i_poi] *= x_base[i_poi] * np.sign(y_base[i_qoi]) / (sys.float_info.epsilon + y_base[i_qoi])
@@ -213,7 +223,8 @@ def get_active_subset(model,lsa_options):
     eliminate=True
     inactive_index=np.zeros(model.n_poi)
     #Calculate Jacobian
-    jac=get_jacobian(model.eval_fcn, model.base_poi, lsa_options, scale=False, y_base=model.base_qoi)
+    jac=get_jacobian(model.eval_fcn, model.base_poi, lsa_options.x_delta,\
+                         lsa_options.method, scale=False, y_base=model.base_qoi)
     while eliminate:
         #Caclulate Fisher
         fisher_mat=np.dot(np.transpose(jac), jac)
@@ -233,6 +244,7 @@ def get_active_subset(model,lsa_options):
         else:
             #Terminate Active Subspace if singular values within tolerance
             eliminate=False
+            
     #Define active and inactive spaces
     active_set=model.name_poi[inactive_index == False]
     inactive_set=model.name_poi[inactive_index == True]