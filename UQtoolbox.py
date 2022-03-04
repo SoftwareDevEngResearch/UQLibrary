Fixed Morris calculation and associated unit tests

UQtoolbox.py
@@ -59,8 +59,8 @@ class Options:
 class Model:
     #Model sets should be initialized with base parameter settings, covariance Matrix, and eval function that
     #   takes in a vector of POIs and outputs a vector of QOIs
-    def __init__(self,base_poi=np.empty(0), name_poi = np.empty(0), \
-                 name_qoi= np. empty(0), cov=np.empty(0), \
+    def __init__(self,base_poi=np.empty(0), name_poi = "auto", \
+                 name_qoi= "auto", cov=np.empty(0), \
                  eval_fcn=np.empty(0), dist_type='uniform', dist_param="auto"):
         #------------------------base_poi, n_poi, name_poi---------------------
         #Assign base_poi and n_poi
@@ -77,34 +77,33 @@ class Model:
         self.n_poi=self.base_poi.size
         
         #Assign name_poi----------------UNFINISHED VALUE CHECKING
+        
+        POInumbers=np.arange(0,self.n_poi)
+        name_poi_auto=np.char.add('POI',POInumbers.astype('U'))
         #Check name_poi is string
         if type(name_poi)==np.ndarray:
             #Check data type
             if name_poi.size!= self.n_poi:
                 raise Exception("Incorrect number of entries in name_poi")
-            self.name_poi=name_poi
         elif type(name_poi)==list:
             #Check data type
             if len(name_poi)!= self.n_poi:
                 raise Exception("Incorrect number of entries in name_poi")
-            self.name_poi=np.array(name_poi)
+            name_poi=np.array(name_poi)
         elif type(name_poi)==str and name_poi.lower()!="auto":
             if self.n_poi!=1:
                 raise Exception("Only one qoi name entered for >1 pois")
-            else :
-                self.name_qoi = name_qoi
         else :
-            POInumbers=np.arange(0,self.n_poi)
-            name_poi=np.char.add('POI',POInumbers.astype('U'))
             if name_poi.lower()!= "auto":
                 warnings.warn("Unrecognized name_poi entry, using automatic values")
+            name_poi = name_poi_auto
         if (name_poi.size != self.n_poi) & (name_poi.size !=0):   #Check that correct size if given
             warnings.warn("name_poi entered but the number of names does not match the number of POIs. Ignoring names.")
             name_poi=np.empty(0)
-        if name_poi.size==0:                                           #If not given or incorrect size, number POIs
-            POInumbers=np.arange(0,self.n_poi)
-            name_poi=np.char.add('POI',POInumbers.astype('U'))
-            
+        if name_poi.size==0:                       
+            name_poi = name_poi_auto
+        self.name_poi = name_poi
+        del name_poi
         #-----------------eval_fcn, base_qoi, n_qoi, name_qoi------------------
         #Assign evaluation function and compute base_qoi
         self.eval_fcn=eval_fcn
@@ -115,34 +114,33 @@ class Model:
         self.n_qoi=len(self.base_qoi)
         
         #Assign name_qoi----------------UNFINISHED VALUE CHECKING
+        #Formulate automatic names so they can be referenced in each case
+        QOInumbers=np.arange(0,self.n_qoi)
+        name_qoi_auto=np.char.add('POI',QOInumbers.astype('U'))
         #Check name_qoi is string
         if type(name_qoi)==np.ndarray:
             #Check data type
             if name_qoi.size!= self.n_qoi:
                 raise Exception("Incorrect number of entries in name_qoi")
-            self.name_qoi = name_qoi
         elif type(name_qoi)==list:
             #Check data type
             if len(name_qoi)!= self.n_qoi:
                 raise Exception("Incorrect number of entries in name_qoi")
-            self.name_qoi=np.array(name_qoi)
+            name_qoi=np.array(name_qoi)
         elif type(name_qoi)==str and name_qoi.lower()!="auto":
             if self.n_qoi!=1:
                 raise Exception("Only one qoi name entered for >1 qois")
-            else: 
-                self.name_qoi = name_qoi
         else :
-            QOInumbers=np.arange(0,self.n_qoi)
-            name_qoi=np.char.add('POI',QOInumbers.astype('U'))
-            if name_poi.lower()!= "auto":
+            if name_qoi.lower()!= "auto":
                 warnings.warn("Unrecognized name_qoi entry, using automatic values")
-        self.name_qoi = name_qoi
-        if (self.name_qoi.size !=self.n_qoi) & (self.name_qoi.size !=0):    #Check names if given match number of QOIs
+            name_qoi= name_qoi_auto
+        if (name_qoi.size !=self.n_qoi) & (name_qoi.size !=0):    #Check names if given match number of QOIs
             warnings.warn("name_qoi entered but the number of names does not match the number of QOIs. Ignoring names.")
-            self.name_qoi = np.empty(0)
-        if self.name_qoi.size==0:                                 #If not given or incorrect size, number QOIs
-            QOInumbers = np.arange(0, self.n_qoi)
-            self.name_qoi = np.char.add('QOI', QOInumbers.astype('U'))
+            name_qoi = name_qoi_auto
+        if name_qoi.size==0:                                 #If not given or incorrect size, number QOIs
+            name_qoi = name_qoi_auto
+        self.name_qoi = name_qoi
+        del name_qoi
             
             
             
@@ -326,15 +324,15 @@ def print_results(results,model,options):
         if options.gsa.run_morris:
             if model.n_qoi==1:
                 print('\n Morris Screening Results for' + model.name_qoi[0])
-                print(tabulate(np.concatenate((model.name_poi.reshape(model.n_poi, 1), results.gsa.mu_star.reshape(model.n_poi, 1), \
-                                               results.gsa.sigma2.reshape(model.n_poi, 1)), 1),
-                    headers=["", "mu_star", "sigma2"]))
+                print(tabulate(np.concatenate((model.name_poi.reshape(model.n_poi, 1), results.gsa.morris_mean_abs.reshape(model.n_poi, 1), \
+                                               results.gsa.morris_std.reshape(model.n_poi, 1)), 1),
+                    headers=["", "mu_star", "sigma"]))
             else:
                 print('\n Morris Screening Results for' + model.name_qoi[iQOI])
                 print(tabulate(np.concatenate(
-                    (model.name_poi.reshape(model.n_poi, 1), results.gsa.mu_star[[iQOI], :].reshape(model.n_poi, 1), \
-                     results.gsa.sigma2[[iQOI], :].reshape(model.n_poi, 1)), 1),
-                    headers=["", "mu_star", "sigma2"]))
+                    (model.name_poi.reshape(model.n_poi, 1), results.gsa.morris_mean_abs[[iQOI], :].reshape(model.n_poi, 1), \
+                     results.gsa.morris_std[[iQOI], :].reshape(model.n_poi, 1)), 1),
+                    headers=["", "mu_star", "sigma"]))
 
 ###----------------------------------------------------------------------------------------------
 ###-------------------------------------Support Functions----------------------------------------

gsa.py
@@ -34,7 +34,7 @@ class GsaOptions:
 class GsaResults:
     #
     def __init__(self,sobol_base=np.nan, sobol_tot=np.nan, f_a=np.nan, f_b=np.nan, f_d=np.nan, f_ab=np.nan, \
-                 samp_d=np.nan, morris_variance=np.nan, morris_mean_abs=np.nan, morris_mean=np.nan):
+                 samp_d=np.nan, morris_std=np.nan, morris_mean_abs=np.nan, morris_mean=np.nan):
         self.sobol_base=sobol_base
         self.sobol_tot=sobol_tot
         self.f_a=f_a
@@ -44,7 +44,7 @@ class GsaResults:
         self.samp_d=samp_d
         self.morris_mean_abs=morris_mean_abs
         self.morris_mean = morris_mean
-        self.morris_variance=morris_variance
+        self.morris_std=morris_std
     pass
 
 
@@ -83,12 +83,12 @@ def run_gsa(model, gsa_options):
         morris_samp = get_morris_poi_sample(model.sample_fcn, gsa_options.n_samp_morris,\
                                             model.n_poi, pert_distance)
             
-        morris_mean_abs, morris_mean, morris_variance = calculate_morris(\
+        morris_mean_abs, morris_mean, morris_std = calculate_morris(\
                                              model.eval_fcn, morris_samp, \
                                              pert_distance)
         gsa_results.morris_mean_abs=morris_mean_abs
         gsa_results.morris_mean = morris_mean
-        gsa_results.morris_variance=morris_variance
+        gsa_results.morris_std=morris_std
 
     #Sobol Analysis
     if gsa_options.run_sobol:
@@ -233,7 +233,7 @@ def calculate_sobol(f_a, f_b, f_ab, f_d):
 
 
 ##--------------------------------calculate_morris-----------------------------
-def calculate_morris(eval_fcn, morris_samp, pert_distance):
+def calculate_morris(eval_fcn, morris_samp, pert_distance, verbose = False):
     """Calculates morris samples using information from Model and GsaOptions objects.
     
     Parameters
@@ -252,42 +252,100 @@ def calculate_morris(eval_fcn, morris_samp, pert_distance):
     """
     #Evaluate Sample
     f_eval_compact = eval_fcn(morris_samp)
+    if verbose:
+        print(f_eval_compact)
     
     #Compute # of pois, qois and samples to ensure consitency
-    n_poi = morris_samp.shape[1]
-    n_samp = morris_samp.shape[0]/(n_poi+1)
-    n_qoi = f_eval_compact.shape[1]
-    
-    #Seperate each parameter search for ease of computation
-    f_eval_seperated = np.empty((n_samp, n_poi+1, n_qoi))
-    for i_samp in range(n_samp):
-        f_eval_seperated[n_samp,:,:] = f_eval_compact[i_samp*(n_poi+1):(i_samp+1)*(n_poi+1),:]
+    if morris_samp.ndim == 1:
+        n_poi = 1
+    elif morris_samp.ndim == 2:
+        n_poi = morris_samp.shape[1]
+    else:
+        raise Exception("More than 2 dimensions in morris_samp")
+    #Convert to int so it can be used in indexing
+    n_samp = int(morris_samp.shape[0]/(n_poi+1))
+    if f_eval_compact.ndim == 2:
+        n_qoi = f_eval_compact.shape[1]
+    elif f_eval_compact.ndim ==1:
+        n_qoi =1
+    else:
+        raise Exception("More than 2 dimensions in f_eval")
         
-    deriv_approx = np.empty((n_samp, n_poi, n_qoi))  # n_samp x n_poi x n_qoi
+    #Uncompact Samples
+    f_eval_seperated = morris_seperate(f_eval_compact, n_samp, n_poi, n_qoi)
+    morris_samp_seperated = morris_seperate(morris_samp, n_samp, n_poi, n_poi)
+    if verbose:
+        print("morris samp seperated: " + str(morris_samp_seperated))
+    
+    #Get which sample perturbs which poi
+    poi_pert_location = get_poi_pert_location(morris_samp_seperated)
+    if verbose:
+        print("poi_pert_location: " + str(poi_pert_location))
+        
+        
+    #initialize data storage arrays with 1 dimension lower if n_qoi =1
+    if n_qoi > 1:
+        deriv_approx = np.empty((n_samp, n_poi, n_qoi))  # n_samp x n_poi x n_qoi
+    else: 
+        deriv_approx = np.empty((n_samp, n_poi))  # n_samp x n_poi
+    if verbose:
+        print("QOIs : " + str(f_eval_seperated))
     
     #Apply finite difference formula 
     #Source: Smith, R. 2011, Uncertainty Quanitification. p.333
-    for i_poi in range(n_poi):
-        deriv_approx[:,i_poi,:] = f_eval_seperated[:,i_poi+1,:] - f_eval_seperated[:,i_poi,:]
+    for i_samp in range(n_samp):
+        for i_pert in range(n_poi): 
+            i_poi = poi_pert_location[i_samp, i_pert]
+            deriv_approx[i_samp,i_poi] = (f_eval_seperated[i_samp,i_pert+1] - \
+                                          f_eval_seperated[i_samp,i_pert])/ pert_distance
+    # for i_poi in range(n_poi):
+    #     deriv_approx[:,i_poi] = f_eval_seperated[:,i_poi+1] - f_eval_seperated[:,i_poi]
+    if verbose:
+        print("deriv approx: " + str(deriv_approx))
         
     #Apply Morris Index formula
     #Source: Smith, R. 2011, Uncertainty Quanitification. p.332
     morris_mean_abs = np.mean(np.abs(deriv_approx),axis = 0) # n_poi x n_qoi
     morris_mean = np.mean(deriv_approx, axis = 0)
-    morris_variance=np.var(deriv_approx, axis=0) # n_poi x n_qoi
+    morris_std=np.sqrt(np.var(deriv_approx, axis=0)) # n_poi x n_qoi
+    
+    if verbose:
+        print("morris mean abs: " + str(morris_mean_abs))
+        print("morris mean abs: " + str(morris_mean))
+        print("morris st: " + str(morris_std))
 
-    return morris_mean_abs, morris_mean, morris_variance
+    return morris_mean_abs, morris_mean, morris_std
 
+def morris_seperate(qoi_compact, n_samp, n_poi, n_qoi):
+    if n_qoi > 1:
+        qoi_seperated = np.empty((n_samp, n_poi+1, n_qoi))
+    else: 
+        qoi_seperated = np.empty((n_samp, n_poi+1))
+    #Seperate each parameter search for ease of computation
+    for i_samp in range(n_samp):
+        qoi_seperated[i_samp] = qoi_compact[i_samp*(n_poi+1):(i_samp+1)*(n_poi+1)]
+        
+    return qoi_seperated
+
+def get_poi_pert_location(morris_samp_seperate):
+    n_samp = morris_samp_seperate.shape[0]
+    n_poi = morris_samp_seperate.shape[2]
+    poi_pert_location = np.empty((n_samp, n_poi))
+    for i_samp in range(n_samp):
+        for i_poi in range(n_poi):
+            poi_pert_location[i_samp,i_poi] = np.argmax(np.abs(morris_samp_seperate[i_samp,i_poi+1] \
+                                          - morris_samp_seperate[i_samp,i_poi]))
+    return poi_pert_location.astype(int)
 ##---------------------------get_morris_poi_sample-----------------------------
 
-def get_morris_poi_sample(param_dist, n_samp, n_poi, pert_distance, random = True):
+def get_morris_poi_sample(param_dist, n_samp, n_poi, pert_distance, random = False):
     #Use sobol distributions for low discrepancy
     #Generate n_samp_morris samples
     random_samp =  param_dist(n_samp)
     #Define Sampling matrices that are constant
     J=np.ones((n_poi+1,n_poi))
     B = (np.tril(np.ones(J.shape), -1))
-    morris_samp = np.empty((n_samp*(n_poi+1), n_poi))
+    morris_samp_compact = np.empty((n_samp*(n_poi+1), n_poi))
     for i_samp in range(n_samp):
         jTheta=random_samp[i_samp,]*J
         #Calculate Morris Sample matrix
@@ -310,8 +368,8 @@ def get_morris_poi_sample(param_dist, n_samp, n_poi, pert_distance, random = Tru
             # Only use non-random formulations for testing matrix generation
             samp_mat = jTheta+pert_distance/2*(np.matmul((2*B-J),D)+J)
         #Stack each grid seach so that a single eval_fcn call is required
-        morris_samp[i_samp*(n_poi+1):(i_samp+1)*(n_poi+1),:] = samp_mat
-    return morris_samp
+        morris_samp_compact[i_samp*(n_poi+1):(i_samp+1)*(n_poi+1),:] = samp_mat
+    return morris_samp_compact
         
 
 ##--------------------------------------GetSampDist----------------------------------------------------

tests/test_integrated.py
@@ -28,7 +28,7 @@ def test_linear_jac_finite():
     options.lsa.method = "finite"
     options.display = False
     
-    model.base_poi = np.array([.5, .5])#np.random.uniform(size=2)
+    model.base_poi = np.array([.5, .5])  #np.random.uniform(size=2)
     
     results = uq.run_uq(model, options)
     
@@ -98,5 +98,17 @@ def test_heat_ident_subset_complex():
 #     assert np.all(true_subset==results.lsa.active_set)
 
 #==============================================================================
-#-------------------------------GSA integrated tests---------------------------
+#----------------------------Morris integrated tests---------------------------
 #==============================================================================
+
+def test_linear_portfolio():
+    (model, options) = UQtoolbox_examples.GetExample("portfolio (normal)")
+    options.gsa.run = True
+    options.lsa.run = False
+    options.gsa.run_sobol = False
+    options.gsa.run_morris = True
+    options.gsa.l_morris = 1/40
+    options.gsa.n_samp_morris = 100
+    
+    results = uq.run_uq(model, options)
+    assert np.allclose(results.gsa.morris_mean_abs, np.array([2, 1]))